dd8d9453356aa97059663c314aa01bc1f48e1056
Yasharm4x <yash4rma@gmail.com>
2025-10-03 02:35:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-const repoName = "CarbonSense-v2"; // ✅ correct variable
+const repoName = "CarbonSense-v3"; // ✅ correct variable
 
 export default defineConfig(({ mode }) => ({
   base: mode === "production" ? `/${repoName}/` : "/", 
